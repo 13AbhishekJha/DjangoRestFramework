8e7a6c950b7562e6e4606aee8f70b022325f9e90
Abhishek Jha <Abhishek@Jha.com>
2021-12-31 21:59:45 +0530

    Added Validation

--- CRUDAPI/api/serializers.py
+++ CRUDAPI/api/serializers.py
@@ -1,8 +1,14 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from .models import Student
 
+#validators
+def start_with_r(value):
+    if value[0].lower()!='r':
+        raise serializers.ValidationError("name should start with R")
+
+
 class StudentSerializers(serializers.Serializer):
-    name=serializers.CharField(max_length=100)
+    name=serializers.CharField(max_length=100,validators=[start_with_r])#you could add multiple validators by adding ,
     roll=serializers.IntegerField()
     city=serializers.CharField(max_length=100)
 
@@ -15,6 +21,22 @@ class StudentSerializers(serializers.Serializer):
         instance.city=validate_data.get('city',instance.city)
         instance.save()
         return instance
+
+    #field level validation
+    def validate_roll(self,value):
+        if value>200:
+            return serializers.ValidationError('Seat Full') 
+        return value       
+
+    #object level validation
+    def validate(self,data):
+        nm=data.get('name')
+        ct=data.get('city')
+        if nm.lower()=='abhi' and ct.lower()!='ranchi':
+            return serializers.ValidationError('City must be Ranchi') 
+        return data    
+
+
 
 
 
