1c4dbd9e989f687c6f018c002334ebca7052e488
Abhishek Jha <Abhishek@Jha.com>
2021-12-31 16:21:10 +0530

    Added Deserilization logic

--- deserialization/api/views.py
+++ deserialization/api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework import serializers
+from .models import Student
+from .serializers import StudentSerializers
+import io
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+@csrf_exempt
+def student_create(request):
+    if request.method=='POST':
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        serializer=StudentSerializers(data=pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res={"msg":"created successfully"}
+            json_data=JSONRenderer().render(res)
+            return HttpResponse(json_data,content_type="application/json")
+        
+        json_data=JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data,content_type="application/json")
+        
