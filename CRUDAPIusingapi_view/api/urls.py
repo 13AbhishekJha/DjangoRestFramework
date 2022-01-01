ade3f47771cc11f23c152b32c876952488afffe4
Abhishek Jha <Abhishek@Jha.com>
2022-01-01 13:44:01 +0530

    Changed api_view

--- CRUDAPIusingapi_view/api/urls.py
+++ CRUDAPIusingapi_view/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("student",views.student_api),
-    path("student/<int:id>",views.student_api_by_ID)
+    path("student/<int:pk>",views.student_api)
 ]
