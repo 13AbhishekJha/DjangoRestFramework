0f4de7972fde919dcaaaa69095b0a8682046a701
Abhishek Jha <Abhishek@Jha.com>
2022-01-03 21:55:06 +0530

    Added folders

--- Authentication/basicAuthentication/api/views.py
+++ Authentication/basicAuthentication/api/views.py
@@ -5,13 +5,17 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
-# Create your views here.
+# Create your views here
+#do all these from admin panel user section
+#user-you need to create and check active
+#Admin-you need to create and check active and staff_status
+#superadmin-all permission.
 
 #using ModelViewSet
 class StudentModelViewSet(viewsets.ModelViewSet):
       queryset=Student.objects.all()
       serializer_class=StudentSerializers
-      # permission_classes=[IsAdminUser] # if you want only staff_status to be true to reach out to api
+      # permission_classes=[IsAdminUser] # check staff_status to reach out to api
 #for globally defining Authentication(i.e when you have more than one viewset and you want to set on all) see settings.py
       # authentication_classes=[BasicAuthentication]
       # permission_classes=[IsAuthenticated]
