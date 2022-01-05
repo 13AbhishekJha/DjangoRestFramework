31a2f607e48581a617c3905f58c43b3220613377
Abhishek Jha <Abhishek@Jha.com>
2022-01-05 17:20:34 +0530

    Added Nested and Hyperlink model

--- SerializerRelations/api/serializers.py
+++ SerializerRelations/api/serializers.py
@@ -9,11 +9,25 @@ class SongSerializers(serializers.ModelSerializer):
         fields=['id','title','singer','duration']
 
 
-class SingerSerializers(serializers.ModelSerializer):
-    # song=serializers.StringRelatedField(many=True,read_only=True)
-    # song=serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name="song-detail")
-    song=serializers.SlugRelatedField(many=True,read_only=True,slug_field="title")
+# class SingerSerializers(serializers.ModelSerializer):
+#     # song=serializers.StringRelatedField(many=True,read_only=True)
+#     # song=serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name="song-detail")
+#     song=serializers.SlugRelatedField(many=True,read_only=True,slug_field="title")
 
+#     class Meta:
+#         model=Singer
+#         fields=['id','name','gender','song']     
+
+
+#Hyper link Model Serializer
+# class SingerSerializers(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model=Singer
+#         fields=['id','url','name','gender']             
+
+#Nested Serializer
+class SingerSerializers(serializers.ModelSerializer):
+    song=SongSerializers(many=True,read_only=True) #name should be related_name parameter
     class Meta:
         model=Singer
-        fields=['id','name','gender','song']        
+        fields=['id','name','gender','song']      
